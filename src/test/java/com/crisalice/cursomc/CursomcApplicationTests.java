283dfafafecd7e2cd87636530307f4c06c78b14d
Cris alice <crgnlvs@gmail.com>
2020-07-20 10:37:16 -0300

    Projeto criado

---
+++ src/test/java/com/crisalice/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crisalice.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
